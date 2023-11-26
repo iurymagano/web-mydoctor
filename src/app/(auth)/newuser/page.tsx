2328e20966b229f514a9e727b5d8280933752e39
iurymagano <iurymagano17@live.com>
2023-11-26 11:47:36 -0300

    upgrade position form newUSer

--- src/app/(auth)/newuser/page.tsx
+++ src/app/(auth)/newuser/page.tsx
@@ -5,9 +5,9 @@ import TeamMedical from "@/assets/gifs/Healthprofessionalteam.gif";
 const NewUser = () => {
   return (
     <div className="h-full w-full">
-      <div style={{ margin: "0 auto" }} className="h-full max-w-6xl px-6">
+      <div style={{ margin: "0 auto" }} className="h-full max-w-6xl  px-6 ">
         <div className="flex h-full w-full items-center pb-12">
-          <div className="flex w-full  items-center gap-14 max-lg:gap-16 ">
+          <div className="flex w-full  items-center gap-14 max-lg:gap-16 max-sm:gap-0">
             <FormNewUser />
             <div className="w-full max-w-[537px]  max-md:hidden">
               {/* <Icon /> */}
